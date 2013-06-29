68d1bd9a5ed084c69a1a80e6c77a963aca0faab8
Alexander <alexandermichaelmiranda@gmail.com>
2013-06-29 14:53:17 -0400

    jquery updates

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ $(document).ready(function() {
 	var totalQuestions = $('.question').size();
 	var currentQuestion = 0;
 	$question = $('.question');
+	$pick_answer = $('.pick_answer');
 	$('.question').hide();
+	$('.pick_answer').hide();
 	$($question.get(currentQuestion)).fadeIn();
 	$('.next').click(function(){
+		if ($('input:checked').length > currentQuestion) {
 		event.preventDefault();
 		$($question.get(currentQuestion)).fadeOut(function(){
 			currentQuestion = currentQuestion + 1;
+			$('.pick_answer').hide();
 			$($question.get(currentQuestion)).fadeIn();
+			})}
+		else {
+			event.preventDefault();
+			$('.pick_answer').show();
+			};
 			
 		});
-		});
+	
 	$('.restart').click(function(){  //Allows user to restart quiz by clicking Restart button
 		event.preventDefault();
 		if(currentQuestion != 0) {   //Prevents script from firing when user is on question 0
