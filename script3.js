537a723a7811742cbd8313ce2b88c63af67ce4ac
Alexander <alexandermichaelmiranda@gmail.com>
2014-01-06 14:20:53 -0800

    More templating work

--- script3.js
+++ script3.js
@@ -3,10 +3,22 @@ function correctCounter(correct) {
 	$('#scores').text('Your current score is: ' + correct + ' out of ' + totalQuestions + '.');
 };
 
+var source = $('#ultimateWin').html();
+var template = Handlebars.compile(source);
+
 $(document).ready(function() {
 	correctCounter();
-	
+	$('#ultimateWin').hide();
+	for (var i = 1; i < data.length; i++) {
+		$('#' + i).hide();
+		console.log("does this fire?");
+		}
 
+	$('.next').on('click', function() {
+		$('#questions-template').hide();
+		$('#ultimateWin').show();
+		console.log("It worked!");
+	})
 
 	
 })
