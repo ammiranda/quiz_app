6adf8d802f282d22e1d730e50a2bb09587efedb2
Alexander <alexandermichaelmiranda@gmail.com>
2014-01-06 14:38:46 -0800

    Fixed correct counter

--- script3.js
+++ script3.js
@@ -7,16 +7,24 @@ var source = $('#ultimateWin').html();
 var template = Handlebars.compile(source);
 
 $(document).ready(function() {
-	correctCounter();
-	$('#ultimateWin').hide();
+	var correct = 0;
+	var answers = [];
+	correctCounter(correct);
 	for (var i = 1; i < data.length; i++) {
 		$('#' + i).hide();
-		console.log("does this fire?");
+		answers.push(data[i - 1].solution);
 		}
+	console.log(answers);
 
-	$('.next').on('click', function() {
-		$('#questions-template').hide();
-		$('#ultimateWin').show();
+	$('.next').on('click', function(e) {
+		var checkedAnswer = $('input:checked').val();
+		e.preventDefault();
+		if (answers.indexOf(checkedAnswer) > -1) {
+			correct += 1;
+			console.log("correct added");
+		}
+		correctCounter(correct);
+		$('#container').append($('#ultimateWin'));
 		console.log("It worked!");
 	})
 
