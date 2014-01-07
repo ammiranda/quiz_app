554b65b4b19b20c037c530b6b362921ab6c3849f
Alexander <alexandermichaelmiranda@gmail.com>
2014-01-07 15:41:21 -0800

    Added restart button functionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,39 @@
 function correctCounter(correct) {
-	var totalQuestions = data.length;
-	$('#scores').text('Your current score is: ' + correct + ' out of ' + totalQuestions + '.');
+	$('#scores').text('Your current score is: ' + correct + ' out of ' + data.length + '.'); // Appends score report to DOM
+};
+
+function quizInitialization() {
+	correct = 0;  // Initializes the correct responses counter
+	counter = 0;  // Initializes the current question counter
+	solutions = [];
+	answers = [];
+
+	correctCounter(correct); // Invokes the correct counter function with the current number of correct responses passed in
+
+	for (var i = 0; i < data.length; i++) {  // Iteration that loads all solutions into the solutions array
+		$('#' + i).hide();
+		solutions.push(data[i].solution);
+		}
+
+	$('#' + counter).show();  // Reveals the question with id that matches the current value of the counter variable
 };
 
 var ult_source = $('#ultimateWin').html();
 var ult_template = Handlebars.compile(ult_source);
 
 $(document).ready(function() {
-	var correct = 0;  // Initializes the correct responses counter
-	var counter = 0;  // Initializes the current question counter
-	var solutions = [];
-	var answers = [];
 
-	correctCounter(correct);
+	quizInitialization();
 
-	for (var i = 0; i < data.length; i++) {
-		$('#' + i).hide();
-		solutions.push(data[i].solution);
-		}
+	$('.restart').on('click', function(e) {  // Restarts the quiz on the click of the restart button
+		e.preventDefault();
 
-	$('#' + counter).show();
+		$('input').prop('checked', false);
+
+		quizInitialization();
+
+	});
+	
 
 	$('.next').on('click', function(e) { // Function that fires on click of the submit button
 
